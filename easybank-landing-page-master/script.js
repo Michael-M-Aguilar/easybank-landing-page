a0062eef31ce6268a77ab5825b96684ad33a9362
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-12-03 16:46:02 -0800

    Added the close svg now for mobile. Also made an addEventList for the close button to help with the overlay and the hamburger. Will clean up more later.

--- easybank-landing-page-master/script.js
+++ easybank-landing-page-master/script.js
@@ -1,20 +1,33 @@
 const $hamburger = document.querySelector('#hamburger');
+const $close = document.querySelector("#close");
 const $body = document.querySelector('#body');
 const $header = document.querySelector('.header');
 const $overlay = document.querySelector('.overlay');
 
 $hamburger.addEventListener('click', function() {
-  // console.log("Hamburger is clicked");
   if($header.classList.contains('open')) {
     $body.classList.remove("noscroll");
     $header.classList.remove('open')
     $overlay.classList.add("unseen");
     $overlay.classList.remove("seen");
+    $close.classList.add('seen');
   }
   else {
     $body.classList.add("noscroll");
     $header.classList.add('open')
     $overlay.classList.remove("unseen");
     $overlay.classList.add("seen");
+    $close.classList.remove('unseen')
+    $hamburger.classList.add("unseen");
   }
 });
+
+$close.addEventListener('click', function() {
+  $close.classList.add('unseen');
+  $close.classList.remove('seen');
+  $hamburger.classList.remove('unseen')
+  $hamburger.classList.add('seen');
+  $overlay.classList.remove('seen');
+  $overlay.classList.add('unseen');
+  $header.classList.remove('open')
+})
